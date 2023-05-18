62983767bb91d5ab6a693d1c887b30484c595e00
“Scablion” <Scablion>
2023-05-18 10:08:48 +0300

    My first git-repo.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("My first git-repo");
+        }
+    }
+}
